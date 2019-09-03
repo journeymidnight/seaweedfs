b1dc716af2898349da272c974ccded485f6b8670
Can Zhang <can@canx.me>
2019-09-03 09:49:52 +0800

    Try direct io

--- weed/storage/volume_create.go
+++ weed/storage/volume_create.go
@@ -4,12 +4,13 @@ package storage
 
 import (
 	"os"
+	"syscall"
 
 	"github.com/chrislusf/seaweedfs/weed/glog"
 )
 
 func createVolumeFile(fileName string, preallocate int64) (file *os.File, e error) {
-	file, e = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
+	file, e = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|syscall.O_DIRECT, 0644)
 	if preallocate > 0 {
 		glog.V(0).Infof("Preallocated disk space for %s is not supported", fileName)
 	}
