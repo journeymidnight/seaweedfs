6a539b4081bfbbc65bb4b84e64beac0748421e2f
can <can@canx.me>
2019-09-23 20:46:36 +0800

    Sleep forever

--- docker/master_and_volume.sh
+++ docker/master_and_volume.sh
@@ -4,7 +4,7 @@ WEED_BIN=/work/seaweedfs/weed/weed
 
 mkdir -p /seaweed_master
 $WEED_BIN master -mdir=/seaweed_master \
-	-ip=$SEAWEED_IP -ip.bind=0.0.0.0 -port=10001 \
+	-ip="$SEAWEED_IP" -ip.bind=0.0.0.0 -port=10001 \
 	-volumePreallocate \
 	-defaultReplication=000 \
 	-volumeSizeLimitMB 1024 \
@@ -17,7 +17,12 @@ do
   mkdir -p /seaweed_volume_$i
   $WEED_BIN volume -dir=/seaweed_volume_$i \
     -mserver=127.0.0.1:10001 \
-    -ip=$SEAWEED_IP -ip.bind=0.0.0.0 -port=3000$i \
+    -ip="$SEAWEED_IP" -ip.bind=0.0.0.0 -port=3000$i \
     -lusfFileSizeGB 1 -max 1 \
-    &> /volume.log
+    &> /volume.log &
+done
+
+while true;
+do
+  sleep 10000;
 done
